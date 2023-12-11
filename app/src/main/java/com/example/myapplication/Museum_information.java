5f649317429662794250c3806cb646edf388ce20
Qian0510 <a98007283@gmail.com>
2023-12-11 22:36:31 +0800

    second commit

---
+++ app/src/main/java/com/example/myapplication/Museum_information.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Museum_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_museum_information);
+    }
+}
