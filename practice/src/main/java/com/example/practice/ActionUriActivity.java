ea6ba3a924fc8b5c65ee0702f20d7c3161ef2498
Qian0510 <a98007283@gmail.com>
2023-12-13 20:26:46 +0800

    second commit

---
+++ practice/src/main/java/com/example/practice/ActionUriActivity.java
@@ -0,0 +1,14 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActionUriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_uri);
+    }
+}
---
+++ practice/src/main/java/com/example/practice/ActsendActivity.java
@@ -0,0 +1,14 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActsendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actsend);
+    }
+}
